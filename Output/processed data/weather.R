c0556afd737db87e434f306c1cb3ba305d411598
emmaheffernan <heffernan.emma@gmail.com>
2016-11-23 17:09:31 +0100

    Delete weather.R

--- Output/processed data/weather.R
+++
@@ -1,5 +0,0 @@
-library("RCurl")
-x <-getURL("https://www.ncdc.noaa.gov/cag/time-series/us/110/00/tavg/all/01/2001-2016.csv")
-weather <- read.csv(text = x)
-weather<-weather[-c(1, 2, 3), ]
-weather <- cbind(year=substr(weather[, 1],1,4), month=substr(weather[, 1],5,6), weather[, -1])
